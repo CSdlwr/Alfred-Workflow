5ac18ea59cf6904e487b11aaf1c6687dcc03bde4
luming.lv <Lv.luming0858@gmail.com>
2016-08-20 13:57:19 +0800

    update youdao translate alfred workflow: refactoring bug fix

--- youdao_translate/translate_1.sh
+++ youdao_translate/translate_1.sh
@@ -1,5 +1,7 @@
 # 调用有道http接口进行翻译
 
+#set -x
+
 function init() {
     jq=/usr/local/bin/jq
     api="http://fanyi.youdao.com/openapi.do?keyfrom=alfred-llm-0858&key=425214878&type=data&doctype=json&version=1.1&"
@@ -20,13 +22,13 @@ function parse_content() {
         echo "Usage: parse_result \"api_content\", exit 100"
         exit 100
     fi
-    translation=$(echo $1 | $jq ".translation | join(\"; \")" | sed -e 's/ //g' -e 's/"$//g')
-    basic_explains=$(echo $api_result | $jq ".basic.explains | join(\"#\")" | sed -e 's/^"//g' -e 's/"$//g')
-    web_kv=$(echo $api_result | $jq "[.web[] | {\"key\": .key, \"value\": .value | join(\"; \")} | .key + \": \" + .value] | join(\"#\")" | sed -e 's/^"//g' -e 's/"$//g')
+    translation=$(echo $1 | $jq ".translation | join(\"; \")" | sed -e 's/^"//g' -e 's/"$//g')
+    basic_explains=$(echo $1 | $jq ".basic.explains | join(\"#\")" | sed -e 's/^"//g' -e 's/"$//g')
+    web_kv=$(echo $1 | $jq "[.web[] | {\"key\": .key, \"value\": .value | join(\"; \")} | .key + \": \" + .value] | join(\"#\")" | sed -e 's/^"//g' -e 's/"$//g')
 }
 
 function print_items() {
-    if [[ $# !=2 ]];then
+    if [[ $# != 2 ]];then
         echo "Usage: print_items \"items title\""
         exit 100
     fi
@@ -41,10 +43,11 @@ function print_items() {
 function display() {
     echo "<?xml version=\"1.0\"?> <items>"
     print_items $translation "youdao translation"
+    echo $IFS
     OLD_IFS=$IFS
-    IFS="#"
-    print_items $basic_explains "youdao dict"
-    print_items $web_kv "web translation"
+    IFS='#'
+    print_items "$basic_explains" "youdao dict"
+    print_items "$web_kv" "web translation"
     IFS=$OLD_IFS
     echo "</items>"
 }
@@ -52,7 +55,7 @@ function display() {
 function main() {
     init
     query_api "$@"
-    parse_content $content
+    parse_content "$content"
     display
 }
 
