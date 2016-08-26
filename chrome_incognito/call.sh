984478e5e892d6beab18fceb037a98db06e0a247
luming.lv <Lv.luming0858@gmail.com>
2016-08-26 17:12:08 +0800

    add google chrome incognito mode

---
+++ chrome_incognito/call.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/osascript
+
+if application "Google Chrome" is running then
+    tell application "Google Chrome" to make new window with properties {mode:"incognito"}
+else
+    do shell script "open -a /Applications/Google\\ Chrome.app --args --incognito"
+end if
