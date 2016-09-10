aa9d67587b4ebda83d3ec2e92d8697423ac761e0
luming.lv <Lv.luming0858@gmail.com>
2016-09-10 20:30:43 +0800

    master: fix find.sh bug in kill_process_by_keyword workflow

--- kill_process_by_keyword/find_1.sh
+++ kill_process_by_keyword/find_1.sh
@@ -11,11 +11,11 @@ if [[ $query != "" ]]; then
 fi
 
 echo $header
-for p in $ps
+for p in $"$ps"
 do
     app_info=$(echo "$p" | awk '{for(i=11; i<=NF; ++i) {printf "%s ", $i} printf "\n"}')
     app_name=$(echo "$app_info" | awk -F '/' '{print $NF}')
     ps_id=$(echo "$p" | awk '{print $2}')
-    echo $(printf "$rt" "$ps_id" "$app_info" "$app_name" "$app_info")
+    echo $(printf "$rt" "$ps_id" "$app_name" "$app_info")
 done
 echo $footer
