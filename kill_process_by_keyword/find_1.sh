c85c5ecead0cfab5ca0ee1ccc6f5271258ec0b91
luming.lv <Lv.luming0858@gmail.com>
2016-09-11 10:45:29 +0800

    master: alfred workflow kill_process_by_keyword: a conciser way of traversing

--- kill_process_by_keyword/find_1.sh
+++ kill_process_by_keyword/find_1.sh
@@ -10,15 +10,23 @@ if [[ $query != "" ]]; then
     ps=$(ps -A -o pid,comm | grep -i "$query")
 fi
 
+#echo $header
+#OLD_IFS=$IFS
+#IFS=$'\n'
+#for p in $ps
+#do
+    #ps_command=$(echo "$p" | awk '{for(i=2; i<=NF; ++i) {printf "%s ", $i} printf "\n"}')
+    #app_name=$(echo "$ps_command" | awk -F '/' '{print $NF}')
+    #ps_id=$(echo "$p" | awk '{print $1}')
+    #echo $(printf "$rt" "$ps_id" "$app_name" "$ps_command")
+#done
+#IFS=$OLD_IFS
+#echo $footer
+
 echo $header
-OLD_IFS=$IFS
-IFS=$'\n'
-for p in $ps
+echo "$ps" | while read ps_id ps_command
 do
-    ps_command=$(echo "$p" | awk '{for(i=2; i<=NF; ++i) {printf "%s ", $i} printf "\n"}')
     app_name=$(echo "$ps_command" | awk -F '/' '{print $NF}')
-    ps_id=$(echo "$p" | awk '{print $1}')
-    echo $(printf "$rt" "$ps_id" "$app_name" "$ps_command")
+    printf "$rt" "$ps_id" "$app_name" "$ps_command"
 done
-IFS=$OLD_IFS
 echo $footer
