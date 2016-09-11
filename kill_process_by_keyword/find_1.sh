9a5d3a56ea620e9c8554f94b34c77bf0e86de28e
luming.lv <Lv.luming0858@gmail.com>
2016-09-11 10:47:02 +0800

    master: alfred workflow kill_process_by_keyword: rm useless comment

--- kill_process_by_keyword/find_1.sh
+++ kill_process_by_keyword/find_1.sh
@@ -10,19 +10,6 @@ if [[ $query != "" ]]; then
     ps=$(ps -A -o pid,comm | grep -i "$query")
 fi
 
-#echo $header
-#OLD_IFS=$IFS
-#IFS=$'\n'
-#for p in $ps
-#do
-    #ps_command=$(echo "$p" | awk '{for(i=2; i<=NF; ++i) {printf "%s ", $i} printf "\n"}')
-    #app_name=$(echo "$ps_command" | awk -F '/' '{print $NF}')
-    #ps_id=$(echo "$p" | awk '{print $1}')
-    #echo $(printf "$rt" "$ps_id" "$app_name" "$ps_command")
-#done
-#IFS=$OLD_IFS
-#echo $footer
-
 echo $header
 echo "$ps" | while read ps_id ps_command
 do
